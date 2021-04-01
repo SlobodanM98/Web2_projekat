c2afe5b3da40c464cbd28373e3c3bece2bd83b42
Dimitrije <dbajagic3@gmail.com>
2021-04-01 16:14:07 +0200

    Social buttons

--- web2-project/src/app/app.module.ts
+++ web2-project/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { OutageComponent } from './outage/outage.component';
+import { LoginComponent } from './components/login/login.component';
+import { OutageComponent } from './components/outage/outage.component';
 @NgModule({
   declarations: [
     AppComponent,
