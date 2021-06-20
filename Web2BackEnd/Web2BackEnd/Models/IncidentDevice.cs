9a9445ba2b675adb7c7556a9eef57562ec14cc6d
Dimitrije <dbajagic3@gmail.com>
2021-06-20 11:30:26 +0200

    one small change

--- Web2BackEnd/Web2BackEnd/Models/IncidentDevice.cs
+++ Web2BackEnd/Web2BackEnd/Models/IncidentDevice.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,7 @@ namespace Web2BackEnd.Models
 {
     public class IncidentDevice
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         public int IncidentID { get; set; }
         public int DeviceID { get; set; }
